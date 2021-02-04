509fa1336dd45d4e1352602202989df2d0b6216a
ddraco-d <a.ed.komarov@gmail.com>
2021-02-04 22:25:26 +0300

    cpp07_errors

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,14 +6,58 @@
 /*   By: ddraco <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 18:42:33 by ddraco            #+#    #+#             */
-/*   Updated: 2021/02/04 18:42:34 by ddraco           ###   ########.fr       */
+/*   Updated: 2021/02/04 22:22:49 by ddraco           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "whatever.hpp"
 
+class Awesome 
+{
+public:
+	Awesome( int n ) : _n( n ) {} 
+	bool operator==( Awesome const & rhs ) const
+	{
+		return (this->_n == rhs._n);
+	}
+	bool operator!=(Awesome const & rhs) const
+	{
+		return (this->_n != rhs._n);
+	}
+	bool operator>(Awesome const & rhs) const 
+	{
+		return (this->_n > rhs._n);
+	}
+	bool operator<(Awesome const & rhs) const 
+	{
+		return (this->_n < rhs._n);
+	} bool operator>=( Awesome const & rhs ) const 
+	{
+		return (this->_n >= rhs._n);
+	}
+	bool operator<=(Awesome const & rhs) const 
+	{
+		return (this->_n <= rhs._n);
+	}
+	int getN(void) {
+		return (this->_n);
+	}
+private:
+	int _n; 
+};
+
 int main( void ) {
+	//Check-list
+	Awesome x(20);
+	Awesome y(120);
+	
+	::swap( x, y );
+	std::cout << "x = " << x.getN() << ", y = " << y.getN() << std::endl;
+	std::cout << "min( x, y ) = " << ::min( x, y ).getN() << std::endl;
+	std::cout << "max( x, y ) = " << ::max( x, y ).getN( ) << std::endl;
+
+	//Subject
 	int a = 2;
 	int b = 3;
 
--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: ddraco <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 19:08:19 by ddraco            #+#    #+#             */
-/*   Updated: 2021/02/04 20:04:47 by ddraco           ###   ########.fr       */
+/*   Updated: 2021/02/04 22:16:46 by ddraco           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ template <typename T>
 class Array
 {
 private:
-	size_t length;
+	unsigned int length;
 	T *arr;
 public:
 	class OutOfBoundsException: public std::exception {
@@ -28,17 +28,22 @@ public:
 		}
 	};
 	virtual ~Array()
-	{}
-	Array() : length(0), arr(nullptr)
-	{}
-	Array(size_t n) : length(n) {
-		arr = new T[n];
+	{
+		if (this->length > 0)
+			delete[] this->arr;
 	}
-	Array(Array<T> const &other) : length(0), arr(nullptr) {
+	Array() : length(0), arr(NULL)
+	{}
+	Array(unsigned int n) : length(n), arr(NULL) {
+		this->arr = new T[n];
+		for (unsigned int i = 0; i < n; i++)
+			this->arr[i] = T();
+	}
+	Array(Array<T> const &other) : length(0), arr(NULL) {
 		if (other.length > 0)
 		{
-			this->arr = new T[other.length];
-			for (size_t i = 0; i < other.length; i++)
+			this->arr = new T[other.length]();
+			for (unsigned int i = 0; i < other.length; i++)
 				this->arr[i] = other.arr[i];
 		}
 		this->length = other.length;
@@ -46,25 +51,25 @@ public:
 	Array &operator=(Array const &other) {
 		if (this->length > 0)
 			delete[] this->arr;
-		this->arr = nullptr;
+		this->arr = NULL;
 		if (other.length > 0)
 		{
 			this->arr = new T[other.length]();
-			for (size_t i = 0; i < other.length; i++)
+			for (unsigned int i = 0; i < other.length; i++)
 				this->arr[i] = other.arr[i];
 		}
 		this->length = other.length;
 		return (*this);
 	}
-	T &operator[](size_t index) {
+	T &operator[](unsigned int index) {
 		if (index >= this->length)
 			throw Array::OutOfBoundsException();
 		return (this->arr[index]);
 	}
-	T const &operator[](size_t index) const {
+	T const &operator[](unsigned int index) const {
 		return (operator[](index));
 	}
-	size_t size(void) const {
+	unsigned int size(void) const {
 		return (this->length);
 	}
 };
